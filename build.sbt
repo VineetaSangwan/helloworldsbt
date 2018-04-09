fed8a2b58df31262237cfa9293bc9ca7a28ab308
Vineeta Sangwan <vineeta.sangwan@go-mmt.com>
2018-04-09 17:47:35 +0530

    initial commit

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "helloworldsbt"
+
+version := "0.1"
+
+scalaVersion := "2.12.5"
+
+
+mainClass in assembly := Some("com.gi.dpt.test.testclass")
---
+++ src/main/scala/com/gi/dpt/test/testclass.scala
@@ -0,0 +1,11 @@
+package com.gi.dpt.test
+
+object testclass {
+  def main(args: Array[String]): Unit = {
+    while(true){
+	println("test class is active now!!!!")
+	Thread.sleep(1000)
+	}
+  }
+
+}
